a7ea773108b554835d19e708ee81d3877e4d83c4
Niklas <n.hakansson@hotmail.com>
2020-11-24 10:00:39 +0100

    Added store.js with some new examples

---
+++ store.js
@@ -0,0 +1,35 @@
+// ---------- store-object ----------
+let ica = {
+  name: "Ica Supermarket Skurup",
+  address: "Drottninggatan 29",
+  zipcode: "12389",
+  city: "Skurup",
+  staff: [],
+  products: [],
+};
+
+// ---------- person-objects ----------
+let niklas = {
+  firstName: "Niklas",
+  lastName: "Håkansson",
+  age: 30,
+  salary: 45000,
+  worksAt: {},
+};
+
+let erik = {
+  firstName: "Erik",
+  lastName: "Jansson",
+  age: 45,
+  salary: 23000,
+  worksAt: {},
+};
+
+let sofia = {
+  firstName: "Sofia",
+  lastName: "Karlsson",
+  age: 22,
+  salary: 60000,
+  worksAt: {},
+};
+
